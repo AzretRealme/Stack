e261926bdfd3ce9a643e5aa50eaab803b08bbf2f
AzretRealme <71967119+AzretRealme@users.noreply.github.com>
2021-03-02 00:47:51 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,25 @@ public class Main {
         auto.push("Porsche");
         auto.push("Ferrari");
 
+        for(String s: auto) {
+            System.out.println(s);
+        }
        /* String a = auto.pop();
         System.out.println(a);*/
-        System.out.println(auto.pop()); //удалить верхний элемент
-        System.out.println(auto.peek()); //распечатать последний элемент после удаления
+        System.out.println("----------------------------");
+
+        System.out.println(auto.pop() + " ---удалить верхний элемент"); //удалить верхний элемент
+        System.out.println(auto.peek() + " ---распечатать последний элемент после удаления"); //распечатать последний элемент после удаления
+
+        System.out.println("----------------------------");
 
         for(String s: auto) {
             System.out.println(s);
         }
+
+        System.out.println("----------------------------");
+
         int p = auto.search("Chevrolet"); //найти позицию первого добавленного элемента
-        System.out.println(p);
+        System.out.println(p + " ---найти позицию первого добавленного элемента");
     }
 }
